06367c6777d5b8584822243dab2f3bd30f078596
anikatasnin <jarintasnin09@gmail.com>
2025-09-18 00:17:41 +0600

    apply style on predict page

---
+++ src/pages/PredictPage.jsx
@@ -0,0 +1,12 @@
+const PredictPage = () => {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">Predict Your Survival</h1>
+      <p className="mt-4 text-lg text-gray-300">
+        Fill your details and see if you survive Titanic!
+      </p>
+    </div>
+  );
+};
+
+export default PredictPage;
